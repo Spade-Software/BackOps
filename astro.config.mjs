6b0b175a7e1d903c37f442126e139fadc1561a49
Evan Henry <evan@spadehq.com>
2025-06-03 15:45:53 -0400

    adds tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,32 +2,41 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'BackOps',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/Spade-Software/BackOps' }],
-			sidebar: [
-				{
-					label: 'Start Here',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'BackOps 101', slug: 'start-here/getting-started' },
-					],
-				},
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
+  integrations: [
+      starlight({
+          title: 'BackOps',
+          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/Spade-Software/BackOps' }],
+          sidebar: [
+              {
+                  label: 'Start Here',
+                  items: [
+                      // Each item here is one entry in the navigation menu.
+                      { label: 'BackOps 101', slug: 'start-here/getting-started' },
+                  ],
+              },
+              {
+                  label: 'Guides',
+                  items: [
+                      // Each item here is one entry in the navigation menu.
+                      { label: 'Example Guide', slug: 'guides/example' },
+                  ],
+              },
+              {
+                  label: 'Reference',
+                  autogenerate: { directory: 'reference' },
+              },
+          ],
+		  customCss: [
+			'./src/styles/global.css',
+		  ],
+      }),
 	],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
